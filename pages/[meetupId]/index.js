ec8998f0d411fb8fb28fb90dd77c2f77b8bf16dd
jordan goulet <jordangoulet13@gmail.com>
2021-09-29 14:30:35 -0700

    before static page generation

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,13 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://images.unsplash.com/photo-1626131719162-a086b0cdd644?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1193&q=80"
+      title="first meetup"
+      description="our first meet up"
+      address="Something street 5"
+    />
+  );
+}
+
+export default MeetupDetails;
